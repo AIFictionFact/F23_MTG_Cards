85118bb15aa8964a91441e7ffce5da75388d9877
EldarGrimm <119770177+EldarGrimm@users.noreply.github.com>
2023-11-19 16:39:27 -0500

    Add files via upload

---
+++ image_generator_v1.py
@@ -0,0 +1,36 @@
+from PIL import Image, ImageDraw, ImageFont
+import requests
+from io import BytesIO
+
+def generate_card_with_art_and_text(image_url, card_name, card_text, output_path='output_card.png'):
+    # Download the image from the provided URL
+    response = requests.get(image_url)
+    image = Image.open(BytesIO(response.content))
+
+    # Create a drawing object
+    draw = ImageDraw.Draw(image)
+
+    # Set font parameters
+    font_path = "c:\\Users\\exrod\Pictures\\school\\rpi\\ai\\project\\magicthegathering\\MAGIC.TTF"  # Replace with the path to a TTF font file
+    font_size = 60
+    font = ImageFont.truetype(font_path, font_size)
+
+    # Set text position
+    text_position_name = (10, 10)
+    text_position_body = (10, 800)
+
+    # Add text to the image
+    draw.text(text_position_name, card_name, font=font, fill="white")
+    draw.text(text_position_body, card_text, font=font, fill="white")
+
+    # Save the combined image with text
+    image.save(output_path)
+
+    print(f"Combined image saved at: {output_path}")
+
+# Example usage:
+card_name = "Fire Guy"
+image_url = "https://oaidalleapiprodscus.blob.core.windows.net/private/org-0WxqUe5jPU7LvfjuLUnB5qlb/user-ux7KFwhjWeqHleMkDLmGO3qt/img-CnFBskpHqlsnfC32a2KxGrXH.png?st=2023-11-19T20%3A02%3A07Z&se=2023-11-19T22%3A02%3A07Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-11-19T20%3A10%3A38Z&ske=2023-11-20T20%3A10%3A38Z&sks=b&skv=2021-08-06&sig=gqrZ2IWKaW2lQNK7gvr%2Bw4b9eAscsZxSBW7pl5TP8Rc%3D"  # Replace with the actual DALL·E image URL
+card_text = "Fire guy do fire thing"
+
+generate_card_with_art_and_text(image_url, card_name, card_text)
---
+++ mtg_card_generator.py
@@ -0,0 +1,65 @@
+import openai
+
+# Set API key
+openai.api_key = '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'
+
+def generate_magic_card_name(card_type, color_identity, mana_value, specialization):
+    # Define a prompt
+    prompt = f"Create only the name of a new Magic: The Gathering card that has the following attributes:\n\nType: {card_type}\nColor Identity: {color_identity}\nMana Value: {mana_value}"
+
+    # Generate card text using the GPT-3 model
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        max_tokens=150,  # Adjust card text length
+        temperature=0.7,  # Adjust randomness
+        n=1  # Number of cards
+    )
+
+    # Extract card text
+    card_text = response.choices[0].text.strip()
+
+    # Print
+    print(card_text)
+    return card_text
+
+
+def generate_magic_card(card_name, card_type, color_identity, mana_value, specialization):
+    # Define a prompt
+    prompt = f"Create the abilities of a new Magic: The Gathering card named {card_name} that has the following attributes:\n\nType: {card_type}\nColor Identity: {color_identity}\nMana Value: {mana_value}\nSpecialization: {specialization}\n\nCard Text:"
+
+    # Generate card text using the GPT-3 model
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        max_tokens=150,  # Adjust card text length
+        temperature=0.7,  # Adjust randomness
+        n=1  # Number of cards
+    )
+
+    # Extract card text
+    card_text = response.choices[0].text.strip()
+
+    # Print
+    print(card_text)
+
+
+def generate_card_art(card_name):
+
+    # Construct a prompt for DALL·E
+    desc = f"Create art for a Magic: The Gathering card named '{card_name}'."
+
+    response = openai.Image.create(
+        model="dall-e-2",
+        prompt=desc,
+        n=1,
+        size="1024x1024"
+    )
+    image_url = response.data[0].url
+
+    print(f"Card Art for '{card_name}': {image_url}")
+
+
+name = generate_magic_card_name("Creature", "Red", "3", "Aggressive")
+generate_magic_card(name, "Creature", "Red", "3", "Aggressive")
+generate_card_art(name)
